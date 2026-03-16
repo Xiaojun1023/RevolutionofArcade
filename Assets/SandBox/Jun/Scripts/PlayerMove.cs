cccf01c88c4e1fb6ea272f847bea4332b639fe5d
Xiaojun1023 <gongxiaojun20021023@gmail.com>
2026-03-15 17:23:12 -0700

    Update the arcade scene, added air walls

--- Assets/SandBox/Jun/Scripts/PlayerMove.cs
+++ Assets/SandBox/Jun/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float gravity = -9.8f;
+    public float jumpHeight = 2f;
 
     private CharacterController controller;
     private float yVelocity;
@@ -24,7 +25,12 @@ public class PlayerMove : MonoBehaviour
 
         if (controller.isGrounded && yVelocity < 0f)
         {
-            yVelocity = -2f; 
+            yVelocity = -2f;
+        }
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            yVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         yVelocity += gravity * Time.deltaTime;
